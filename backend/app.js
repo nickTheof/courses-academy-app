68eac0582889673b8db51d4833550cf535ace1f2
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-04-25 23:57:11 +0300

    feat(backend/error middleware): add error middleware to handle all thrown errors. Handle known possible different types of errors using Business App Error for differentiate them in production environment

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const errorController = require("./middlewares/error.middleware");
 
 const app = express();
+
+// Add global error middleware
+app.use(errorController);
 
 module.exports = app;
